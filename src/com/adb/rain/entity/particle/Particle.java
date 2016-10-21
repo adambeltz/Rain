e84af012ede75b41889df01a95edfcf6fa00dfd6
Adam Beltz <adambeltz@hotmail.com>
2016-10-21 15:45:30 -0500

    finish 80

--- src/com/adb/rain/entity/particle/Particle.java
+++ src/com/adb/rain/entity/particle/Particle.java
@@ -10,7 +10,8 @@ public class Particle extends Entity{
 
     private int life;
     private int time = 0;
-    protected double xx, yy, xa, ya;
+    protected double xx, yy, zz;
+    protected double xa, ya, za;
 
     public Particle(int x, int y, int life){
 
@@ -22,8 +23,11 @@ public class Particle extends Entity{
         sprite = Sprite.particle_normal;
 
 
-        this.xa = random.nextGaussian();
+        this.xa = random.nextGaussian() + 1.8;
+        if (this.xa < 0) xa = 0.1;
         this.ya = random.nextGaussian();
+        this.zz = random.nextFloat() + 2.0;
+
     }
 
 
@@ -33,15 +37,24 @@ public class Particle extends Entity{
         time++;
         if (time >= 7400) time = 0;
         if (time > life) remove();
+        za -= 0.1;
 
+        if (zz < 0){
+            zz = 0;
+            za *= -.55;
+            xa *= 0.4;
+            ya *= 0.4;
+        }
 
         this.xx += xa;
         this.yy += ya;
+        this.zz += za;
+
 
     }
 
     public void render(Screen screen){
-        screen.renderSprite((int)xx, (int)yy, sprite, true);
+        screen.renderSprite((int)xx - 14, (int)yy - (int)zz, sprite, true);
 
     }
 }
